59d8d79c05dd34caf664e9b703c8254b7fc92970
Gravetow <wruszczak@web.de>
2018-10-08 00:00:04 +0200

    Added upwards of raycast hit code

--- Bachelor/Assets/ViveController.cs
+++ Bachelor/Assets/ViveController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ViveController : MonoBehaviour
 {
+    private static bool teleportActive;
+
     private SteamVR_TrackedController viveWand;
     private LineRenderer lineRenderer;
     private bool padTouched;
+
+    public GameObject TeleportIndicator;
 
     // Use this for initialization
     private void Start()
@@ -26,26 +30,50 @@ public class ViveController : MonoBehaviour
 
     private void OnPadTouched(object sender, ClickedEventArgs e)
     {
-        padTouched = true;
+        if (!teleportActive)
+        {
+            teleportActive = true;
+            padTouched = true;
+            TeleportIndicator.SetActive(true);
+        }
     }
 
     private void OnPadUntouched(object sender, ClickedEventArgs e)
     {
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, transform.position);
+        TeleportIndicator.SetActive(false);
         padTouched = false;
+        teleportActive = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        transform.position = viveWand.transform.position;
+        Vector3 currentPosition = viveWand.transform.position;
+        Vector3 forwardPosition = viveWand.transform.forward * 50;
+
+        transform.position = currentPosition;
         transform.rotation = viveWand.transform.rotation;
 
         if (padTouched)
         {
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, transform.forward * 10);
+            lineRenderer.SetPosition(0, currentPosition);
+            lineRenderer.SetPosition(1, forwardPosition);
+
+            CheckForHit(currentPosition, forwardPosition);
         }
+    }
+
+    private bool CheckForHit(Vector3 start, Vector3 direction)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(start, direction, out hit))
+        {
+            TeleportIndicator.transform.position = hit.point;
+            TeleportIndicator.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            return true;
+        }
+        return false;
     }
 }

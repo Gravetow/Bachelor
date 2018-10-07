fe170963f96a52a3872d122de6ec7bf7c662012e
Gravetow <wruszczak@web.de>
2018-10-07 20:49:19 +0200

    Added custom tracking

---
+++ Bachelor/Assets/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public Camera Headcamera;
+
+    private void Start()
+    {
+        transform.position = Headcamera.transform.position;
+    }
+}
---
+++ Bachelor/Assets/ViveController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveController : MonoBehaviour
+{
+    private SteamVR_TrackedObject viveWand;
+
+    // Use this for initialization
+    private void Start()
+    {
+        viveWand = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = viveWand.transform.position;
+    }
+}

7f1c3c440db16a48f5992c66c58c14c4eeba2cbe
Gravetow <wruszczak@web.de>
2018-10-07 22:43:04 +0200

    Added Lines

--- Bachelor/Assets/ViveController.cs
+++ Bachelor/Assets/ViveController.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class ViveController : MonoBehaviour
 {
-    private SteamVR_TrackedObject viveWand;
+    private SteamVR_TrackedController viveWand;
+    private LineRenderer lineRenderer;
+    private bool padTouched;
 
     // Use this for initialization
     private void Start()
     {
-        viveWand = GetComponent<SteamVR_TrackedObject>();
+        viveWand = GetComponent<SteamVR_TrackedController>();
+        lineRenderer = GetComponent<LineRenderer>();
+
+        viveWand.PadTouched += OnPadTouched;
+        viveWand.PadUntouched += OnPadUntouched;
+    }
+
+    private void OnDestroy()
+    {
+        viveWand.PadTouched -= OnPadTouched;
+        viveWand.PadUntouched -= OnPadUntouched;
+    }
+
+    private void OnPadTouched(object sender, ClickedEventArgs e)
+    {
+        padTouched = true;
+    }
+
+    private void OnPadUntouched(object sender, ClickedEventArgs e)
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position);
+        padTouched = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
         transform.position = viveWand.transform.position;
+        transform.rotation = viveWand.transform.rotation;
+
+        if (padTouched)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.forward * 10);
+        }
     }
 }

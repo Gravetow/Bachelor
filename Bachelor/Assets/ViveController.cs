937e6555958cb58f4081a54c403b389d30a580e9
Gravetow <wruszczak@web.de>
2018-10-08 01:32:41 +0200

    Added more teleporting

--- Bachelor/Assets/ViveController.cs
+++ Bachelor/Assets/ViveController.cs
@@ -8,7 +8,11 @@ public class ViveController : MonoBehaviour
 
     private SteamVR_TrackedController viveWand;
     private LineRenderer lineRenderer;
+    private RaycastHit hit;
     private bool padTouched;
+    public Vector3 targetTeleportPosition;
+
+    private MeshRenderer currentMesh;
 
     public GameObject TeleportIndicator;
 
@@ -20,12 +24,19 @@ public class ViveController : MonoBehaviour
 
         viveWand.PadTouched += OnPadTouched;
         viveWand.PadUntouched += OnPadUntouched;
+        viveWand.PadClicked += OnPadClicked;
     }
 
     private void OnDestroy()
     {
         viveWand.PadTouched -= OnPadTouched;
         viveWand.PadUntouched -= OnPadUntouched;
+        viveWand.PadUntouched -= OnPadClicked;
+    }
+
+    private void OnPadClicked(object sender, ClickedEventArgs e)
+    {
+        transform.parent.position = targetTeleportPosition;
     }
 
     private void OnPadTouched(object sender, ClickedEventArgs e)
@@ -55,11 +66,15 @@ public class ViveController : MonoBehaviour
 
         transform.position = currentPosition;
         transform.rotation = viveWand.transform.rotation;
+    }
 
+    private void LateUpdate()
+    {
+        Vector3 currentPosition = viveWand.transform.position;
+        Vector3 forwardPosition = viveWand.transform.forward * 50;
         if (padTouched)
         {
             lineRenderer.SetPosition(0, currentPosition);
-            lineRenderer.SetPosition(1, forwardPosition);
 
             CheckForHit(currentPosition, forwardPosition);
         }
@@ -67,13 +82,35 @@ public class ViveController : MonoBehaviour
 
     private bool CheckForHit(Vector3 start, Vector3 direction)
     {
-        RaycastHit hit;
         if (Physics.Raycast(start, direction, out hit))
         {
             TeleportIndicator.transform.position = hit.point;
             TeleportIndicator.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+            lineRenderer.SetPosition(1, hit.point);
+            if (hit.collider.GetType() == typeof(BoxCollider))
+            {
+                if (currentMesh == null)
+                {
+                    currentMesh = hit.collider.GetComponent<MeshRenderer>();
+                    currentMesh.enabled = true;
+                }
+                else if (currentMesh.gameObject != hit.collider.gameObject)
+                {
+                    currentMesh.enabled = false;
+                    currentMesh = hit.collider.GetComponent<MeshRenderer>();
+                    targetTeleportPosition = currentMesh.transform.position;
+                    currentMesh.enabled = true;
+                }
+            }
+            else
+            {
+                targetTeleportPosition = hit.point;
+            }
             return true;
         }
+        lineRenderer.SetPosition(1, direction);
+
         return false;
     }
 }

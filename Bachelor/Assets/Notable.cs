a58ecc6a3f29d0e75d9a076108afa9588dd7a18a
Gravetow <wruszczak@web.de>
2018-11-07 08:45:32 +0100

    Added Trellosupport

--- Bachelor/Assets/Notable.cs
+++ Bachelor/Assets/Notable.cs
@@ -2,19 +2,61 @@
 using DG.Tweening;
 using UnityEngine;
 using Zenject;
+using Trello;
 
 public class Notable : MonoBehaviour, IInputClickHandler
 {
     public InteractiveMeshCursor cursor;
     public GameObject NoteInput;
 
+    public TrelloSend trelloSend;
+
     [Inject]
     public ModeSettings settings;
 
-    public void OnInputClicked(InputClickedEventData eventData)
+    [Inject]
+    public readonly SignalBus signalBus;
+
+    private string cardName;
+
+    private void Start()
+    {
+        signalBus.Subscribe<ButtonPressedSignal>(ShowNote);
+    }
+
+    private void OnDestroy()
+    {
+        signalBus.Unsubscribe<ButtonPressedSignal>(ShowNote);
+    }
+
+    public void ShowNote()
     {
         if (settings.currentMode != Mode.NOTE) return;
 
         NoteInput.SetActive(true);
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        //if (settings.currentMode != Mode.NOTE) return;
+
+        //NoteInput.SetActive(true);
+    }
+
+    public void SaveCardName(string title)
+    {
+        cardName = title;
+    }
+
+    public void SaveToTrello(string description)
+    {
+        // Create a new Trello card
+        var card = new TrelloCard
+        {
+            name = "test",
+            desc = "description"
+        };
+
+        trelloSend.SendNewCard(card);
     }
 }

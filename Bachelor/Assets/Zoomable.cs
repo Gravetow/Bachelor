eea580147b9f16f620f5b5ade204b12bf2099868
Gravetow <wruszczak@web.de>
2018-11-07 06:36:59 +0100

    Implemented zoom

---
+++ Bachelor/Assets/Zoomable.cs
@@ -0,0 +1,23 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using HoloToolkit.Unity.InputModule;
+
+public class Zoomable : MonoBehaviour, IInputHandler
+{
+    public InteractiveMeshCursor cursor;
+    private Vector3 startPosition;
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        startPosition = Camera.main.transform.position;
+        Camera.main.transform.parent.DOMove((cursor.Position - startPosition) * 0.25f, 0.5f);
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        Camera.main.transform.parent.DOMove(startPosition, 0.5f);
+    }
+}

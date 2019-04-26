7818386d4953b104242d651c29dfa2d2c20d7cca
Gravetow <wruszczak@web.de>
2019-04-26 05:55:20 +0200

    Added Slider number

--- Bachelor/Assets/0_Final/Scripts/VRList/FilterSlider.cs
+++ Bachelor/Assets/0_Final/Scripts/VRList/FilterSlider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,7 +8,11 @@ using Zenject;
 
 public class FilterSlider : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField]
+    private TextMeshProUGUI text;
+
     private Slider slider;
+
     [Inject] private SignalBus _signalBus;
 
     private void Awake()
@@ -23,5 +28,10 @@ public class FilterSlider : MonoBehaviour, IPointerClickHandler
 
         slider.value = (clickPosition.x + 75) / 1.5f;
         _signalBus.Fire(new FilterBySliderSignal() { amount = slider.value });
+    }
+
+    private void Update()
+    {
+        text.text = "Size > " + slider.value;
     }
 }

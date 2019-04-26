3d9f3fde810c9aa251d5d6232fea0c3c5a0de8a1
Gravetow <wruszczak@web.de>
2019-04-26 04:20:13 +0200

    Fixed minor issues and implement slider

---
+++ Bachelor/Assets/0_Final/Scripts/ManipulationTool/FilterSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FilterSlider : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.LogError(eventData.position);
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
